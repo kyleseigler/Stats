c11f657b78f913fc62b84430c0b365471bd3b71c
Kyle Seigler <me@kyleseigler.net>
2014-06-30 01:51:32 -0400

    Modified systemUsers.php to list currently logged-in users, as well as the total number.

--- scripts/systemUsers.php
+++ scripts/systemUsers.php
@@ -1,4 +1,10 @@
 <?php
-$systemUsers = shell_exec("uptime | gawk '{print $6}'");
-print("$systemUsers users");
+$systemUserTotal = shell_exec("uptime | gawk '{print $6}'");
+$systemUsersList = shell_exec("users");
+
+$systemUsersPrepped = str_replace(' ', ', ',$systemUsersList);
+$systemUsersTrimmed = substr($systemUsersPrepped, 0, -1);
+
+print("$systemUserTotal users ($systemUsersTrimmed)");
+
 ?>
