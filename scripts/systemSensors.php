1373602426a856d1b47eafd8e1b911b138674b1f
Kyle Seigler <me@kyleseigler.net>
2014-06-29 23:35:04 -0400

    Added loose files (index.html?, systemSensors.php, systemUptime.php).

--- scripts/systemSensors.php
+++ scripts/systemSensors.php
@@ -1,9 +1,9 @@
 <?php
 $sensorsCore0 = shell_exec("sensors | egrep 'Core 0' | awk '{print $3}'");
-print("Core 0: $sensorsCore0");
+print("Core 0: $sensorsCore0&deg;C<br>");
 
 $sensorsCore1 = shell_exec("sensors | egrep 'Core 1' | awk '{print $3}'");
-print("Core 1: $sensorsCore1");
+print("Core 1: $sensorsCore1&deg;C<br>");
 
 $sensorsFan = shell_exec("sensors | egrep 'Master' | awk '{print $3}'");
 print("Fan speed: $sensorsFan RPM");
--- scripts/systemUptime.php
+++ scripts/systemUptime.php
@@ -1,4 +1,4 @@
 <?php
-$systemUptime = shell_exec("uptime -p");
+$systemUptime = shell_exec("uptime -p | awk '{print $2,$3,$4,$5,$6,$7}'");
 print("$systemUptime");
 ?>
