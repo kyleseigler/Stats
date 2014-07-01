8f572ac8297521884b4430b43e2d2ad4c7fb44b0
Kyle Seigler <me@kyleseigler.net>
2014-07-01 00:04:44 -0400

    Added README.md, and lengthened 'gawk' command in systemUptime.php to display all four fields in 'Uptime' section.

--- scripts/systemUptime.php
+++ scripts/systemUptime.php
@@ -1,4 +1,4 @@
 <?php
-$systemUptime = shell_exec("uptime -p | awk '{print $2,$3,$4,$5,$6,$7}'");
+$systemUptime = shell_exec("uptime -p | gawk '{print $2,$3,$4,$5,$6,$7,$8,$9}'");
 print("$systemUptime");
 ?>
